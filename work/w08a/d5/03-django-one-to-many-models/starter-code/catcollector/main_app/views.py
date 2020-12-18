14d95253b8c92c8a23ecc3d972ab916ad46e4ddd
Jim Clark <jim.clark@generalassemb.ly>
2020-12-18 13:11:49 -0800

    Update

--- work/w08a/d5/03-django-one-to-many-models/starter-code/catcollector/main_app/views.py
+++ work/w08a/d5/03-django-one-to-many-models/starter-code/catcollector/main_app/views.py
@@ -2,17 +2,7 @@ from django.shortcuts import render
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Cat
 
-class CatCreate(CreateView):
-  model = Cat
-  fields = '__all__'
-
-class CatUpdate(UpdateView):
-  model = Cat
-  fields = ['breed', 'description', 'age']
-
-class CatDelete(DeleteView):
-  model = Cat
-  success_url = '/cats/'
+# View functions
 
 def home(request):
   return render(request, 'home.html')
@@ -27,3 +17,15 @@ def cats_index(request):
 def cats_detail(request, cat_id):
   cat = Cat.objects.get(id=cat_id)
   return render(request, 'cats/detail.html', { 'cat': cat })
+
+class CatCreate(CreateView):
+  model = Cat
+  fields = '__all__'
+
+class CatUpdate(UpdateView):
+  model = Cat
+  fields = ['breed', 'description', 'age']
+
+class CatDelete(DeleteView):
+  model = Cat
+  success_url = '/cats/'
